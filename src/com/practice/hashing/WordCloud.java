1e09d648874be7aef84e9ad170583fb59ce4a5d2
Aurobinda Mondal <die4java@gmail.com>
2019-08-23 03:00:52 -0400

    updated

--- src/com/practice/hashing/WordCloud.java
+++ src/com/practice/hashing/WordCloud.java
@@ -2,9 +2,7 @@ package com.practice.hashing;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -12,6 +10,25 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;	
 
+
+/*
+ * You want to build a word cloud, an infographic where the size of a word corresponds 
+ * to how often it appears in the body of text.
+ * To do this, you'll need data. Write code that takes a long string and builds its word cloud data in a hash map ↴ , 
+ * where the keys are words and the values are the number of times the words occurred.
+ * 
+ * Think about capitalized words. For example, look at these sentences:
+ * 
+ * "After beating the eggs, Dana read the next step:"
+ * "Add milk and eggs, then add flour and sugar."
+ * 
+ * What do we want to do with "After", "Dana", and "add"? In this example, 
+ * your final hash map should include one "Add" or "add" with a value of 22. 
+ * Make reasonable (not necessarily perfect) decisions about cases like "After" and "Dana".
+ * 
+ * Assume the input will only contain words and standard punctuation.
+ * 
+ */
 public class WordCloud {
 
 	public static class WordCloudData {
